592fa9556440edf270eed7ec30c6d4f3816eb58c
Eric-Ericsson <eokpoti19@gmail.com>
2024-12-03 09:21:21 +0000

    Fixed search params error messages

--- app/ClientComponent.tsx
+++ app/ClientComponent.tsx
@@ -1,19 +1,22 @@
+"use client";
+
 import CategoriesList from "@/components/home/CategoriesList";
 import PropertiesContainer from "@/components/home/PropertiesContainer";
 
-type HomePageProps = {
+function ClientComponent({
+  searchParams,
+}: {
   searchParams: { category?: string; search?: string };
-};
-
-async function HomePage({ searchParams }: HomePageProps) {
-  const { category, search } = await searchParams;
+}) {
+  const category = searchParams?.category || "";
+  const search = searchParams?.search || "";
 
   return (
-    <section>
+    <>
       <CategoriesList category={category} search={search} />
       <PropertiesContainer category={category} search={search} />
-    </section>
+    </>
   );
 }
 
-export default HomePage;
+export default ClientComponent;
